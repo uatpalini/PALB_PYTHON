3f1003fabf45aa20448132781768e83ced61bb40
uatpalini <136169249+uatpalini@users.noreply.github.com>
2026-05-22 00:25:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Previous Smaller Element/previous-smaller-element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def prevSmaller(self, arr):
+        st = []
+        res = []
+
+        for x in arr:
+
+            while st and st[-1] >= x:
+                st.pop()
+
+            if st:
+                res.append(st[-1])
+            else:
+                res.append(-1)
+
+            st.append(x)
+
+        return res
