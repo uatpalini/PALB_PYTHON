2d529ab8beb47e84529ffd7c225268722e9e9f7e
uatpalini <136169249+uatpalini@users.noreply.github.com>
2026-05-21 22:40:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Rotate Array by One/rotate-array-by-one.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotate(self, arr):
+        last = arr[-1]
+        
+        for i in range(len(arr) - 1, 0, -1):
+            arr[i] = arr[i - 1]
+        
+        arr[0] = last
+        
+        return arr
