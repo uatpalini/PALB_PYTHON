e3f2a2196594eb5737215c870783063364348f59
uatpalini <136169249+uatpalini@users.noreply.github.com>
2026-05-21 23:54:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxWater(self, arr):
+        n = len(arr)
+
+        left = 0
+        right = n - 1
+
+        leftMax = 0
+        rightMax = 0
+
+        water = 0
+
+        while left <= right:
+
+            if arr[left] <= arr[right]:
+
+                if arr[left] >= leftMax:
+                    leftMax = arr[left]
+                else:
+                    water += leftMax - arr[left]
+
+                left += 1
+
+            else:
+
+                if arr[right] >= rightMax:
+                    rightMax = arr[right]
+                else:
+                    water += rightMax - arr[right]
+
+                right -= 1
+
+        return water
